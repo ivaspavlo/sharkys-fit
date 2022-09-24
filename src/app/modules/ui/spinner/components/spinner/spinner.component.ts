2796ff19f7809c4965d7065052cff45c2e5457d1
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2022-09-24 14:06:51 +0200

    ST-13: Implement progress spinner for button

---
+++ src/app/modules/ui/spinner/components/spinner/spinner.component.ts
@@ -0,0 +1,14 @@
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-spinner',
+  templateUrl: './spinner.component.html',
+  styleUrls: ['./spinner.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SpinnerComponent {
+
+  @Input() size: 'sm' | 'md' | 'lg' = 'md';
+
+}
