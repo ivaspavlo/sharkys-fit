0793d7c8eed088863261e3478cf85f020b580d99
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2022-11-01 09:38:08 +0100

    ST-59: Implement guard before cancelling trainer

--- src/app/modules/lazy/admin/services/admin.service.ts
+++ src/app/modules/lazy/admin/services/admin.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Injector } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import { catchError, map, switchMap, tap } from 'rxjs/operators';
 import { SpinnerService } from '@core/services';
 import { IResponseApi } from '@app/interfaces';
 import { ApiService } from '@app/shared/classes';
@@ -27,9 +28,9 @@ export class AdminService extends ApiService {
           data: res
         }
       }),
-      catchError((res: any) => of({
+      catchError((res: HttpErrorResponse) => of({
         valid: false,
-        error_message: res.error_message
+        error_message: res.error.error_message
       })),
       tap(() => this.spinnerService.off())
     );
@@ -44,36 +45,38 @@ export class AdminService extends ApiService {
           data: res
         };
       }),
-      catchError((res: any) => of({
+      catchError((res: HttpErrorResponse) => of({
         valid: false,
-        error_message: res.error_message
+        error_message: res.error.error_message
       })),
       tap(() => this.spinnerService.off())
     );
   }
 
   public cancelTrainer(id: string): Observable<IResponseApi> {
-    this.spinnerService.on();
-    return this.delete<any>('admin/trainers', { body: { id }}).pipe(
-      map((res: any) => ({
-        valid: true
-      })),
-      catchError((res: any) => of({
-        error_message: res.error_message,
-        valid: false
-      })),
-      tap(() => this.spinnerService.off())
+    return of(null).pipe(
+      tap(() => this.spinnerService.on()),
+      switchMap(() => this.delete<any>('admin/trainers', { body: { id }}).pipe(
+        map(() => ({
+          valid: true
+        })),
+        catchError((res: HttpErrorResponse) => of({
+          error_message: res.error.error_message,
+          valid: false
+        })),
+        tap(() => this.spinnerService.off())
+      ))
     );
   }
 
   public approveTrainer(id: string = '1'): Observable<IResponseApi> {
     this.spinnerService.on();
     return this.post('admin/trainers', { status: 'approved', id }).pipe(
-      map((res: any) => ({
+      map(() => ({
         valid: true
       })),
-      catchError((res: any) => of({
-        error_message: res.error_message,
+      catchError((res: HttpErrorResponse) => of({
+        error_message: res.error.error_message,
         valid: false
       })),
       tap(() => this.spinnerService.off())
